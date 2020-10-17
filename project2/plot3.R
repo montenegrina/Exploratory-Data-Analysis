a1daffac775c32274a351dd8f0e7d4052518e7aa
montenegrina <sokovic.anamarija@gmail.com>
2020-10-17 13:24:57 -0500

    Create plot3.R

---
+++ project2/plot3.R
@@ -0,0 +1,19 @@
+library("data.table")
+library("ggplot2")
+setwd("/Users/ams/Downloads/exdata_data_NEI_data")
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Subset NEI data by Baltimore
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
